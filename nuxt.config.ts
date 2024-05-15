235bd76e7960104fd00b0d988a4e13f111b1a290
FurmanDenis <denys.f@overdose.digital>
2024-05-15 13:06:06 +0300

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,20 +8,6 @@ export default defineNuxtConfig({
 
   // Define the build directory
   buildDir: resolve(__dirname, '.nuxt'),
-
-  // Global page headers
-  head: {
-    title: 'My Nuxt App',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
   // Global CSS
   css: [],
 
